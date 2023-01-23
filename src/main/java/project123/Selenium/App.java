12f5588aa96ef3935ebb5126a41a6045a071df06
venkatanagasai <venkatanagasai448@gmail.com>
2023-01-23 15:23:41 +0530

    Sai

---
+++ src/main/java/project123/Selenium/App.java
@@ -0,0 +1,13 @@
+package project123.Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
